72d5635b8443ea0b2f1be3d1c8bce02a42262be3
ivanatomic43 <ivana.tomic96@gmail.com>
2020-05-05 02:37:07 +0200

    Changes

---
+++ ApartmentAir/WebContent/JavaScriptFiles/HomePage.js
@@ -0,0 +1,19 @@
+
+function showLoginForm(){
+
+	$("#loginForm").show();
+	$("#registrationForm").hide();
+	$("#headerForm").hide();
+	
+}
+
+ function showRegistrationForm(){
+	
+
+	$("#registrationForm").show();
+	$("#loginForm").hide();
+	$("#headerForm").hide();
+	
+	
+}
+
